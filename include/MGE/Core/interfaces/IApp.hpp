f0fe40bb158faa0d33530fd5aee01a179b13cf09
Ostkaka <tomasberglund005@gmail.com>
2012-10-02 00:08:48 +0200

    Began coding IApp.hpp

--- include/MGE/Core/interfaces/IApp.hpp
+++ include/MGE/Core/interfaces/IApp.hpp
@@ -1,9 +1,9 @@
 /*
-* This is a useless header for an iapp interface
+* This is the abstract app class in MGE that is used for building applications
 */
-
 #ifndef   CORE_APP_HPP
 	#define   CORE_APP_HPP
+
 #include <fstream>
 #include <stddef.h>
 #include <string.h>
@@ -19,7 +19,7 @@ namespace MGE
 	{
 		public:
 
-			/*
+			/**
 			* App constructor
 			*/
 			IApp();
@@ -27,14 +27,101 @@ namespace MGE
 			/**
 			* App Deconstructor
 			*/
-			~IApp();
+			virtual ~IApp();
 
 			/**
-			* Returns a random number
+			* Will return the most recent created IApp based class. Used for retrieving 
+			* assets among other things
 			*/
-			int getNumber();
+			static IApp * getApp();
+
+			/*
+			* Processes the command line for arguments 
+			* @param[in] argc is the number of arguments
+			* @param[in] argv are the actual arguments
+			*/
+			virtual void ProcessArguments(int argc, char* argv[]);
+
+			/**
+			*
+			*@return Valued of the exit-code given by the application by using quit.
+			*/
+			int run();
+
+			/**
+			* Used to check in case the application is still running
+			* @return True if the application is running or False if it still is
+			*/
+			bool isRunnning();
+
+			/*
+			* 
+			*
+			*/
+			//setGraphicsRange()
+
+			/**
+       * Returns the current game loop update rate being
+       * used.
+       * @return update rate in Hz (updates per second)
+       */
+      float getUpdateRate(void) const;
+
+      /*
+       * Set the maximum number of sequential updates
+       * allowed in any given game loop. If your FPS is
+       * struggling, you should set theMaxUpdates to 1.
+       * @param[in] theMaxUpdates range is [1,200]
+       */
+      //void setMaxUpdates(Uint32 theMaxUpdates);
+
+
+			void quit(int exitCode = 1);
+
 
 		protected:
+
+      /**
+       * Basic constructor
+       * @param[in] Title of the window
+       */
+      IApp(const std::string theTitle = "MGE Application");
+
+			/**
+       * Registers custom IAssetHandler derived classes for a specific game application.
+			 *
+       */
+      virtual void initAssetHandlers() = 0;
+
+      /**
+       * Responsible for monitoring IsRunning and exiting when the
+       * Application is done.
+       */
+      virtual void gameLoop();
+
+      /**
+       * ProcessInput is responsible for performing all input processing for
+       * the game loop.
+       */
+      virtual void processInput(IState& theState);
+
+      /**
+       * Responsible for performing any custom last minute
+       * Application cleanup steps before exiting the Application.
+       */
+      virtual void cleanup(void) = 0;
+
+		  ///CONSTANTS
+			/// Default Video width
+			static const unsigned int DEFAULT_VIDEO_WIDTH = 800;
+			/// Default Video height 
+			static const unsigned int DEFAULT_VIDEO_HEIGHT = 600;
+			/// Default video bits per pixel (color depth) 
+			static const unsigned int DEFAULT_VIDEO_BPP = 32;
+			/// Default application wide settings file string
+			static const char* APP_SETTINGS;
+
+			///Variables
 		  /// Title to use for Window
 		  std::string               mTitle;
 		  /// Video Mode to use (width, height, bpp)
